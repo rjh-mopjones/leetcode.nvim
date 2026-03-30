4cd7e33c9701059d03d2b4ad83ed32011288d824
rjh-mopjones <roryhedderman@gmail.com>
2026-03-30 10:52:21 +0100

    auto sign-in from Chrome cookies instead of manual paste

--- lua/leetcode-ui/group/page/signin.lua
+++ lua/leetcode-ui/group/page/signin.lua
@@ -16,10 +16,37 @@ page:insert(header)
 
 page:insert(Title({}, "Sign in"))
 
-local problems = Button("Sign in (By Cookie)", {
+local problems = Button("Sign in (From Chrome)", {
     icon = "󱛖",
     sc = "s",
-    on_press = cmd.cookie_prompt,
+    on_press = function()
+        local cookie = require("leetcode.cache.cookie")
+        local script = vim.fn.expand("~/term_prof/scripts/chrome_leetcode_cookie.py")
+        vim.fn.jobstart({ "python3", script }, {
+            stdout_buffered = true,
+            on_stdout = function(_, data)
+                local cookie_str = table.concat(data, "")
+                if cookie_str == "" then return end
+                vim.schedule(function()
+                    local err = cookie.set(cookie_str)
+                    if not err then
+                        require("leetcode.logger").info("Sign-in successful")
+                        cmd.start_user_session()
+                    else
+                        require("leetcode.logger").error("Sign-in failed: " .. err)
+                    end
+                end)
+            end,
+            on_stderr = function(_, data)
+                local err = table.concat(data, "")
+                if err ~= "" then
+                    vim.schedule(function()
+                        require("leetcode.logger").error("Cookie extraction failed: " .. err)
+                    end)
+                end
+            end,
+        })
+    end,
 })
 
 local exit = ExitButton()
